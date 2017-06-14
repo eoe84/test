3881150b5d6a67045177bbd492a7f68294bae801
Edu <eduardooceteescribano@gmail.com>
2017-06-14 18:57:09 +0100

    asd

---
+++ carpeta/ProjectFacade.java
@@ -0,0 +1,17 @@
+package com.sap.supportframework.project.facade;
+
+import com.sap.supportframework.project.dto.ProjectDTO;
+import com.sap.supportframework.project.model.Project;
+
+public interface ProjectFacade 
+	int cambio en el master
+	public Project createProject(ProjectDTO project);
+	
+	int
+	
+	public void updateProject(Project project,  ProjectDTO newProject);
+	
+	
+	public void updateProject(Project project, ProjectDTO newProject);
+
+}
