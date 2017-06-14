5a1103e80e5393460536223d293562fd33b5bdcb
Edu <eduardooceteescribano@gmail.com>
2017-06-14 18:55:00 +0100

    4 commit

---
+++ carpeta/ProjectFacade.java
@@ -0,0 +1,12 @@
+package com.sap.supportframework.project.facade;
+
+import com.sap.supportframework.project.dto.ProjectDTO;
+import com.sap.supportframework.project.model.Project;
+
+public interface ProjectFacade 
+	int cambio en el master
+	public Project createProject(ProjectDTO project);
+	
+	public void updateProject(Project project, ProjectDTO newProject);
+
+}
